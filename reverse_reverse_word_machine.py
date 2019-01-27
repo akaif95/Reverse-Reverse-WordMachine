a88066cf3de1be9321425ab2d01f95d2a71c0c42
Ali Kaif <aliakaif@email.arizona.edu>
2019-01-27 11:59:47 -0700

    Add files via upload

---
+++ reverse_reverse_word_machine.py
@@ -0,0 +1,9 @@
+#This program is like the reverse_machine program
+#Except this time it is designed to take a sentence that has been
+#Been reversed and put it back the way it was before
+
+reversed_variable = input("""Please input the reversed word, phrase or sentence:
+-----------------------------------------------------------------------------------
+""")
+decoded_variable = reversed_variable[::-1]
+print(decoded_variable)
